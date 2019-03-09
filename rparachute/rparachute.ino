4eb2654ccdff6d8a82403ea7160f8ab94cf80892
masaki <oksongh@gmail.com>
2019-03-09 20:45:15 +0900

    電源電圧を測定するコードの追加

--- rparachute/rparachute.ino
+++ rparachute/rparachute.ino
@@ -117,7 +117,7 @@ SensorInfo ReadSensors() {
 
   // バッテリー電圧の測定
   sensors.battery_arduino = (float)analogRead(PIN_BATTERY_ARDUINO)*BATTERY_ARDUINO_COEFFICIENT;
-  sensors.battery_servo = (float)analogRead()*BATTERY_SERVO_COEFFICIENT;
+  sensors.battery_servo = (float)analogRead(PIN_BATTERY_SERVO)*BATTERY_SERVO_COEFFICIENT;
 
   // コマンドの読み取り
   SerialReadCommand(&sensors);
